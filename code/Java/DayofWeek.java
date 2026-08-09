1550217592cec6a87f8cf96d35e25cb966f9668b
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-09 21:49:23 +0600

    Create Day of the Week program with user input handling
    Implement a console application to determine the day of the week based on user input.

---
+++ code/Java/DayofWeek.java
@@ -0,0 +1,36 @@
+package pk1;
+import java.util.Scanner;
+public class task4 {
+   public static void main(String[] args) {
+	   Scanner scanner=new Scanner(System.in);
+	   int a =scanner.nextInt();
+	   int num=a%7;
+	   switch(num){
+	   case 0:
+		   System.out.println("Saturday");
+		   break;
+	   case 1:
+		   System.out.println("Sunday");
+		   break;
+	   case 2 :
+		   System.out.println("Monday");
+		   break;
+	   case 3:
+		   System.out.println("Tuesday");
+		   break;
+	   case 4:
+		   System.out.println("Wednesday");
+		   break;
+	   case 5:
+		   System.out.println("Thusday");
+		   break;
+	   case 6:
+		   System.out.println("Friday");
+		   break;
+		  
+		   default:
+			   System.out.println("Invalid");
+	   }
+		   
+}
+}
