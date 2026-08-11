997a64ed19bd787b1622c68e3e89e107e74e2baa
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-11 21:38:53 +0600

    Add factorial calculation program in Java

---
+++ code/Java/factorial.java
@@ -0,0 +1,30 @@
+package pkg1;
+
+import java.util.Scanner;
+
+public class TASK9 {
+
+    
+    public static long factorial(int n) {
+       
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+      
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        if (n < 0) {
+            System.out.println("Factorial is only applicable for positive number");
+        } else {
+            System.out.println(factorial(n));
+        }
+
+        scanner.close();
+    }
+}
