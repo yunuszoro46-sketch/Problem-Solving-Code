2e8191f8617ba688047fe726e277a03e10be7484
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-14 12:01:12 +0600

    Add TASK7 to sum even digits of a number
    Implement a program to sum even digits of an integer input.

---
+++ code/Java/task7.java
@@ -0,0 +1,31 @@
+package pkg1;
+import java.util.Scanner;
+public class TASK7 {
+
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	   
+	        int number = scanner.nextInt();
+
+	        int sum = 0;
+	        int temp = Math.abs(number);
+
+	    
+	        while (temp > 0) {
+	            int digit = temp % 10; 
+
+	            if (digit % 2 == 0) {
+	                sum += digit; 
+	            }
+
+	            temp = temp / 10; 
+	        }
+	      
+	        System.out.println(sum);
+
+	        scanner.close();
+	    }
+	}
+
