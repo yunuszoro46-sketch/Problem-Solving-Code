d565efe3d6d3809e486593b625adc995c9b5e9a6
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-16 02:48:07 +0600

    Add UapCse class with main method for CGPA calculation

---
+++ JavaOOP/CGPA/UapCse.java
@@ -0,0 +1,9 @@
+package PKG1;
+
+public class UapCse {
+    public static void main(String[] args) {
+		cse student1 = new cse("Younus","25101109",30,2.85);
+		student1.updateCgpa(3.0, 4.00);
+		student1.displayInfo();
+	}
+}
