f68443cae23995057e9ddd6888ed539d04e7cc96
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-18 03:05:29 +0600

    Implement user input for bank account operations
    Refactored main method to accept user input for account creation and operations.

--- JavaOOP/BankAccount/Bank.java
+++ JavaOOP/BankAccount/Bank.java
@@ -1,11 +1,52 @@
 package PKG1;
-
+import java.util.Scanner;
 public class Bank {
     public static void main(String[] args) {
-		BankAcc acc1 = new BankAcc("younus", "25101109", 10000 );
-		acc1.withdraw(2000);
-		acc1.display();
-		acc1.deposit(1000);
-		System.out.println(acc1.getBalance());
+		Scanner scanner = new Scanner(System.in);
+		
+		 System.out.println("Enter your name: ");
+	     String name = scanner.nextLine();
+	     System.out.println("Enter your ID: ");
+	     String id = scanner.nextLine();
+	     System.out.println("Enter balance: ");
+	     double balance = scanner.nextDouble();
+		
+		  BankAcc bang = new BankAcc(name,id,balance);
+		int option=-1;
+		while(option!=0) {
+			System.out.println("i. Input ‘1’ to deposit money.\n"
+					+ "ii. Input ‘2’ to withdraw money.\n"
+					+ "iii. Input ‘3’ to display the balance of the account.\n"
+					+ "iv. Input ‘4’ to display the details of the account.\n"
+					+ "v. Input ‘0’ to exit the system. ");
+			System.out.println("Enter your choice: ");
+			option=scanner.nextInt();
+			
+			switch(option) {
+			case 1:
+			    System.out.println("Enter the ammount you want to deposit: ");
+			    double depo = scanner.nextDouble();
+			    bang.deposit(depo);
+				break;
+			case 2:
+				System.out.println("Enter withdraw ammount: ");
+				double  with = scanner.nextDouble();
+			    bang.withdraw(with);
+			    break;
+			case 3:
+				System.out.println("Balance ammount: "+bang.getBalance());
+				
+				break;
+			case 4:
+				System.out.println("account details: ");
+				bang.display();
+				break;
+			case 0:
+				System.out.println("Exiting goodbye :) ");
+				break;
+			 default:
+				 System.out.println("invalid");
+			}
+		}
 	}
 }
