7c943335a48700537701e30d09d294c1c59dfcfb
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-20 00:26:59 +0600

    Rename class from task2 to evenodd

--- code/Java/evenodd.java
+++ code/Java/evenodd.java
@@ -1,6 +1,6 @@
 package pk1;
 import java.util.Scanner;
-public class task2 {
+public class evenodd {
    public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter a number: ");
