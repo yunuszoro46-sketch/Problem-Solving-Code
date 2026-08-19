4de05f58646607782978288d01132b0ed5401f36
Yunus Been Hashem <yunuszoro46@gmail.com>
2026-08-20 00:52:30 +0600

    Fix: make public class name match filename (task7) to resolve CI javac error

--- code/Java/task7.java
+++ code/Java/task7.java
@@ -1,31 +1,30 @@
 package pkg1;
 import java.util.Scanner;
-public class TASK7 {
+public class task7 {
 
 
-	    public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	   
-	        int number = scanner.nextInt();
+       
+        int number = scanner.nextInt();
 
-	        int sum = 0;
-	        int temp = Math.abs(number);
+        int sum = 0;
+        int temp = Math.abs(number);
 
-	    
-	        while (temp > 0) {
-	            int digit = temp % 10; 
+    
+        while (temp > 0) {
+            int digit = temp % 10; 
 
-	            if (digit % 2 == 0) {
-	                sum += digit; 
-	            }
+            if (digit % 2 == 0) {
+                sum += digit; 
+            }
 
-	            temp = temp / 10; 
-	        }
-	      
-	        System.out.println(sum);
+            temp = temp / 10; 
+        }
+      
+        System.out.println(sum);
 
-	        scanner.close();
-	    }
-	}
-
+        scanner.close();
+    }
+}
